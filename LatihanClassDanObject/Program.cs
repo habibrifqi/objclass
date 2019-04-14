5d985f39d7754973fbfd272991b412c4698d7484
habibrifqi <habib.rifqi.16@gmail.com>
2019-04-14 18:55:52 +0700

    project dua

---
+++ LatihanClassDanObject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanClassDanObject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari class Mahasiswa
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            // mengeset nilai properties objek saya dan kamu
+            saya.Nim = "12345";
+            saya.Nama = "Rifqi";
+            saya.Ipk = 3.99f;
+
+            kamu.Nim = "54321";
+            kamu.Nama = "Error";
+            kamu.Ipk = 3.55f;
+
+            //memangil method registrasi dan isi krs
+            saya.Registrasi();
+            saya.IsiKrs();
+
+            kamu.Registrasi();
+            kamu.IsiKrs();
+
+            Console.ReadKey();
+        }
+    }
+}
